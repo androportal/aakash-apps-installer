c0515f19393056b2045bbfbab800fcb15917722a
Srikant <usp71084@gmail.com>
2012-11-16 11:50:21 +0530

    created a separate repository for all apps installer, just to avoid confusion

---
+++ README.rst
@@ -0,0 +1,179 @@
+README
+======
+
+What is this app for ?
+----------------------
+
+* Will detect the device and show its MAC address for easy documentation
+
+* Will auto sync with the ftp server for latest updated applications
+
+* Install all apps and data files to respective directories
+
+* Detect the MAC address of the device and update the same in a ``csv`` file on the Desktop
+
+Requires
+--------
+
+* curlftpfs
+
+* rsync
+
+* adb
+
+* If you are on 64 bit machine then install ia-32 libs, as ``adb`` only comes
+  for x86 machines
+
+* Also requires internal LAN connection to ftp server
+
+
+How to install ?
+----------------
+
+The alien way::
+
+        $ bash install.sh
+
+A easier way would be using ``deb`` package, which contains all the dependencies
+``(curlftpfs, rsync, adb)`` except ia-32 libs
+
+The link of ``deb`` file will be uploaded soon
+
+Also note that for offline installation you don't require ``curlftpfs`` and ``rsync``
+
+
+How to use this ?
+-----------------
+
+Open terminal and type ``aakash`` on the prompt. Connect the aakash with USB cable
+to your Linux machine. That's all, observe the installation messages on the screen. 
+There will be a directory name ``aakash`` will be created on the Desktop which 
+contain all synced data and apks from ftp server. If you try to change in ``~/Desktop/aakash``
+directory it won't work out because when you run the installer it will remove any local 
+modifications and sync with the ftp server again.
+
+Optionally you can turn ``ON`` your aakash's WiFi to view its MAC address. No wireless
+connection is required, turning ``ON`` the WiFi is sufficient. 
+
+
+Application flags:
+------------------
+
+1. This will start the application, and will ask for your sudo password ::
+
+        $ aakash     
+       
+#. Same as above but without ftp sever check, force install(-f) or offline install::
+
+        $ aakash -f
+              
+#. Only get the MAC address(-m) ::
+
+        $ aakash -m
+                     
+#. Only push apk's, skip the data(-a) ::
+
+        $ aakash -a
+                            
+#. Show this help(-h) ::
+
+        $ aakash -h  
+
+#. Add new ftp site(add) ::
+
+        $ aakash -add ftp://address/aakash
+        
+#. Show full offline help in web browser(-hb) with your choice of browser ::
+
+        $ aakash -hb firefox
+
+
+How to upload my apps on the ftp server(for aakash developers only) ?
+---------------------------------------------------------------------
+
+1. Let us assume we have a ftp URL as : ``ftp://127.0.0.1/aakash`` on web browser
+
+#. Keep ``aakash`` directory in the root of your ftp setup, say ``/srv/ftp/aakash``, in case of 
+   offline installation setup create a directory ``aakash`` on the ``Desktop``, it should look 
+   like ``/home/<your username>/Desktop/aakash``, in following example replace ``/srv/ftp/aakash``
+   with ``/home/<your username>/Desktop/aakash`` if you wish to continue offline setup.
+   Also remember you should run as ``aakash -f`` in case of offline installation.
+
+#. Please create a directory of your application name inside ``/srv/ftp/aakash``
+
+#. Let us take an example for ``clicker`` application ::
+
+         mkdir -p /srv/ftp/aakash/clicker
+
+#. Now create two optional subdirectories inside ``/srv/ftp/aakash/clicker`` which will 
+   hold the ``apk`` and ``data`` (if any)::
+
+        mkdir -p /srv/ftp/aakash/clicker/apk
+        mkdir -p /srv/ftp/aakash/clicker/data
+
+
+#. If your application doesn't require any ``data`` then you may not create the ``data``
+   directory.
+
+
+#.  Now create a plain text file ``path_of_apks_and_data`` in ``aakash`` directory reflecting
+    your application's apk and data file path(if any). Sample file content for above example is 
+    shown, this will be same for both online and offline installation ::
+    
+        aakash/clicker/apk,aakash/clicker/data
+
+
+#. Add more applications to ``path_of_apks_and_data`` in separate lines
+
+#. Now copy your ``apk(s)`` to ``/srv/ftp/aakash/clicker/apk`` and make sure you name them
+   according to some version(not necessary but recommended) ::
+
+        cp Clicker.apk /srv/ftp/aakash/clicker/apk
+
+   Now if your application doens't need any data files then you can stop here. You have 
+   successfully uploaded your ``apk`` on to ftp server. Now others can install it remotely.
+
+
+#. Now with ``data``. To put your data files(eg: xml, videos, scripts etc) in 
+   ``/srv/ftp/aakash/clicker/data`` follow the previous approach, i.e copy them to 
+   data directory first. Please remember ``data`` can be a file or a directory, the procedure
+   will be same for both ::
+
+        cp -r data-directory/ /srv/ftp/aakash/clicker/data/
+        cp  myfile /srv/ftp/aakash/clicker/data/
+
+
+#. Now as your data files/directories are copied inside the data directory,
+   the next step would be to tell the installer application that where you want to 
+   push them in ``aakash tablet(for eg: /mnt/sdcard/clicker)``.
+   
+#. To do so, create a plain text file and name it as ``path``. Open this file in your favorite 
+   editor and add your source file/directory name and destination path(aakash's path). The only
+   precaution you must take is to add a trailing ``/`` at the end of the directory name. 
+
+   Sample content of ``path`` file as per our previous example ::
+
+        data-directory/,/mnt/sdcard/data-directory/
+        myfile,/data/local/myfile
+
+   
+   Note that you have to mention same ``data-directory/`` and ``myfile`` in destination path.
+   That's it, save this file and copy it to ``/srv/ftp/aakash/clicker/data/`` ::
+
+
+        cp path /srv/ftp/aakash/clicker/data/
+
+
+   At this point your ``/srv/ftp/aakash/clicker/data/`` directory contains 2 files (myfile, path)
+   and one directory (data-directory/)
+
+
+#. Everytime you change your data file or directory you should also reflect it  in ``path`` file
+
+
+As ``path`` file is a ``csv`` file (comma separated file), so please do not add any 
+additional characters in this file. Start from the first line first character.
+
+
+For any query contact developer through github or email.
+
---
+++ aakash-installer.py
@@ -0,0 +1,319 @@
+#!/usr/bin/python
+
+import os
+import csv
+import sys
+import time
+import subprocess
+from subprocess import Popen, PIPE
+
+mac_cmd = "sudo adb shell ip link show wlan0 | busybox awk '/ether/ {print $2}'"
+adb_cmd = "sudo adb get-state"
+curlftpfs_mount_dir = " /mnt/aakash "
+curlftpfs_cmd = "sudo curlftpfs  %s"  + curlftpfs_mount_dir 
+rsync_dest_dir = " ~/Desktop/ "
+rsync_cmd = "sudo rsync -ra --delete " + curlftpfs_mount_dir + rsync_dest_dir
+umount_dir = "sudo umount " + curlftpfs_mount_dir 
+# unset proxy didn't work, need to find some better solution
+unset_proxy = "unset http_proxy https_proxy ftp_proxy"
+mac_addr = ''
+logFileName = ''
+flag_state = 1
+mac_count = 0
+
+
+
+def headerText():
+    os.system('clear')
+    print '\n'
+    print '----------------------------------------------------------'
+    print '|      Aakash  Installation  Log     (Version 1.0)       |' 
+    print '----------------------------------------------------------\n\n'
+
+
+def deviceFailureText():
+    print "      ------------------------------------------"
+    print "      |        *** NO DEVICE FOUND ***         |"
+    print "      | Connect aakash and wait for 10 seconds |"
+    print "      ------------------------------------------"
+
+
+def statusText():
+    print "\t     ------------------------------"
+    print "\t     |  Aakash device connected   |"
+    print "\t     ------------------------------\n"
+
+
+def footerText():
+    print "\n\n========================================================================================"
+    print "| If you wish to stop this program type 'Control + c' to exit                          |"
+    print "| There are more options available, check aakash installer help by typing: aakash -h   |"
+    print "========================================================================================"
+    print "\n\n========================================================================================================"
+    print "|   Task complete. Remove the USB cable and connect another device, don't cancel this program          |" 
+    print "========================================================================================================"
+
+
+def help():
+    os.system('cat /usr/share/aakash/help.txt')
+
+
+def getStdout(command):
+    # returns stdout of command as string 
+    return Popen(command, shell=True, stdout=PIPE).stdout.read().strip('\n')
+
+
+def rsyncWithServer():
+    if not os.path.isfile(os.getenv('HOME') + '/.aakash'):
+        print "\n\n It seems you are running this program for first time. We need to know"
+        print "your preferred ftp site to sync. Run"
+        print "\n\t $ aakash -add ftp://your-ftp-site/aakash\n"
+        print "Contact your system admin if you don't have one."
+        sys.exit(0)
+    else:
+        ftp = open(os.getenv('HOME') + '/.aakash')
+        ftp_addr = ftp.read().strip('\n')
+    print "-->  Trying to contact %s for any update" %(ftp_addr) +'\n'
+    subprocess.call(umount_dir,stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True) 
+    subprocess.call("sudo mkdir -p %s %s" %(curlftpfs_mount_dir, rsync_dest_dir),\
+                    stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
+    # A test must be done whether ftp is running or not, if not rsync should not take place
+    # as it will empty the ~/Desktop/aakash directory which is in sync with /mnt/aakash
+    # if error code (0 means no error)
+    if subprocess.call(curlftpfs_cmd %(ftp_addr),stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True):
+        print "Error connecting to ftp server, please check your URL & connection."
+        print "If you are very sure that there is nothing to sync from server and you want to "
+        print "force install the apps then use 'aakash' with '-f'(force install) flag:\n"
+        print "$ aakash -f"
+        print "\nNot recommended."
+        print "\nQuitting application !"
+        sys.exit(0)
+    else:
+        print "-->  Server found and ready for sync."
+    if subprocess.call(rsync_cmd,stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True):
+        print "\n\nftp server detected, but failed to sync. Please install 'rsync' and try again !"
+        print "Quitting application !"
+        sys.exit(0)
+    else:
+        print "\n-->  Syncing in progress. Keep patience. If you are syncing for first time it may take a while."
+        
+    subprocess.call(umount_dir,stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
+    os.system('sudo sync')
+    print "\n-->  Syncing with server done, all latest apps present in ~/Desktop/aakash/ \n"
+    time.sleep(4)
+
+
+def updateListOfServerDirs():
+    allServerPaths = []
+    os.chdir(os.getenv('HOME') + '/Desktop/aakash/')
+    if os.path.isfile('path_of_apks_and_data'):
+        reader = csv.reader(open('path_of_apks_and_data', 'r'))
+        for row in reader:
+            allServerPaths.append(row)
+    else:
+        print '\n\nError, ' + os.getenv('HOME') + '/Desktop/aakash/path_of_apks_and_data' + " not found !"
+        print "This is a rare occassion, ask your system admin to set aakash in the root of the ftp server."
+        print "\nQuitting application !"
+        sys.exit(0)
+    return allServerPaths       
+
+def installAPKs():
+    # converting relative paths to absolute for present user
+    fullPathofAPK = []
+    list_of_apk_dirs = []
+    # allServerPaths contain a array of paths of both apk and data dirs of ~/Desktop/aakash
+    allServerPaths = updateListOfServerDirs()
+    for eachPath in allServerPaths:
+        list_of_apk_dirs.append(eachPath[0])
+    for eachdir in list_of_apk_dirs:
+        fullPathofAPK.append(os.getenv('HOME') + '/Desktop/' + eachdir)
+    for eachFullPath in fullPathofAPK:
+        if os.path.isdir(eachFullPath):
+            os.chdir(eachFullPath)
+            print "\n\nInstalling apks from :  %s" %(eachFullPath)
+            print "----------------------\n"
+            for apks in os.listdir("."):
+                if apks.endswith(".apk"):
+                    if subprocess.call("sudo adb install -r %s" %(apks),\
+                                        stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True):
+                        print "-->  Can't install %s, please check if\
+                               device is connected properly\n" %(apks)
+                        print "Quitting !"       
+                        sys.exit(0)       
+                    else:
+                        print "-->  Installed successfully %s\n" %(apks)
+
+
+def checkAndroidDirExistenceIfNotCreate(path):
+    # As 'path' is full path so need to separate source & destination from csv row
+    if '/' in path[0]:
+        subprocess.call("adb shell busybox mkdir -p %s" %(path[1]),\
+                        stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
+
+
+def pushData():
+    # Will read 'path' file and push data to android
+    fullPathofData = []
+    list_of_data_dirs = []
+    # allServerPaths contain a array of paths of both apk and data dirs of ~/Desktop/aakash
+    allServerPaths = updateListOfServerDirs()
+    for eachPath in allServerPaths:
+        list_of_data_dirs.append(eachPath[1])
+    for eachdir in list_of_data_dirs:
+        fullPathofData.append(os.getenv('HOME') + '/Desktop/' + eachdir)
+    for eachDataPath in fullPathofData:
+        if os.path.isdir(eachDataPath):
+            os.chdir(eachDataPath)
+            print "\n\nPushing data from :  %s" %(eachDataPath)
+            print "------------------\n"
+            if os.path.isfile('path'):
+                reader = csv.reader(open('path', 'r'))
+                for row in reader:
+                    # Must create dir if not present (make it default way)
+                    checkAndroidDirExistenceIfNotCreate(row)
+                    # on success os.system returns 0, so checking it state
+                    if subprocess.call("sudo adb push %s" %('\t'.join(row)),stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True):
+                        print "-->  Can't push file to destination,"
+                        print "     please check if you have sufficient space in destination !\n"
+                        print "     Quitting application !"
+                        sys.exit(0)
+                    else:
+                        print "-->  Pushed %s to %s successfully \n" %(row[0], row[1])
+
+
+def macIdLog(mac_addr):
+    global logFileName, flag_state, mac_count
+    # this will run only first time   
+    if flag_state:
+        logFileName = time.strftime("%a_%d_%b_%Y_%H_%M_%S") + '.csv'
+        flag_state = 0
+    with open(os.getenv('HOME') + '/Desktop/' + logFileName, 'a') as csvfile:
+        macLog = csv.writer(csvfile)
+        if mac_addr in open(os.getenv('HOME') + '/Desktop/' + logFileName).read():
+            print "\n-->  ERROR ERROR ERROR !!!"
+            print "     MAC Address already present, don't repeat devices,"
+            print "     this instance is not updated in csv file\n"
+        else:    
+            macLog.writerow([mac_addr])
+            print "-->  MAC address updated successfully in %s !\n" \
+            %(os.getenv('HOME') + '/Desktop/' + logFileName)
+            #making a copy for worst case scenario
+            mac_count = mac_count + 1
+            print "-->  Total MAC addresses updated in csv file so far : %s\n" %(mac_count)
+            os.system("sudo cp %s /usr/share/aakash" %(os.getenv('HOME') +\
+            '/Desktop/' + logFileName)) 
+
+def checkDeviceMacAddress():
+    # Read the mac address with the mac_cmd functions
+    print "-->  Turning on Wifi, please wait... \n"	
+    subprocess.call("adb shell svc wifi enable", 
+		    stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
+    time.sleep(5)	
+    mac_addr = getStdout(mac_cmd)
+    if int(len(mac_addr)) is 17:
+        print '-->  MAC address of the device :  ' + mac_addr + '\n'
+	macIdLog(mac_addr)
+    else:
+        headerText()
+        statusText()
+        print '-->  **NOTE :  Automatic Wifi detection failed, to view MAC address of the device just enable WiFi in Aakash,'
+        print '          connection to WiFi is not required.\n'
+    # Setting time
+    os.environ['TZ'] = 'Asia/Kolkata'
+    subprocess.call("adb shell date -s %s" %(time.strftime("%Y%m%d.%H%M%S")),\
+                     stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
+    print "-->  Fixed system time, but can not fix the time zone.\n" 
+    subprocess.call("adb shell svc wifi disable", 
+		    stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True)
+    print "-->  Wifi tunrd off\n"	
+
+
+def detectDevice():
+    while True:
+        time.sleep(0.5)
+        # Checking the adb output for the following string, when detected the welcome
+        # header text is handled in checkDeviceMacAddress function, just to save lines
+        if 'device' in getStdout(adb_cmd):
+            headerText()
+            statusText()
+            break    
+        else:
+            headerText()
+            deviceFailureText()
+
+
+def executeAll(*exceptThese):
+    # All apps in default order present in listFunctions[], this
+    # allows user to skip one or functions based on args
+    listFunctions = ['detectDevice()', 'checkDeviceMacAddress()',
+                     'rsyncWithServer()', 'installAPKs()',
+                     'pushData()','footerText()']
+    # Converting the tuple into list                     
+    exceptThese = list(exceptThese)
+    # If length is 0 means no args i.e run aakash in default mode
+    if len(exceptThese) is 0:
+        for eachFunction in listFunctions:
+            exec(eachFunction)
+    else:
+    # Checking functions in exceptThese & removing them    
+        for eachSkipFunction in exceptThese:
+            listFunctions.remove(eachSkipFunction)                            
+    # Executing new listFunctions with removed functions        
+        for eachFunction in listFunctions:
+            exec(eachFunction)
+
+
+def waitForNewDevice():
+    # Checking whether the cable is unplugged by waiting for adb response
+    while True:
+        time.sleep(1)
+        if 'unknown' in getStdout(adb_cmd):
+            break
+
+
+def  processArgs():
+    args = sys.argv
+    # All flags are resloved here, '-f' for force install, skipping the server sync
+    if '-f' in args:
+        executeAll('rsyncWithServer()')
+    # Skipping all, will show only mac address    
+    elif '-m' in args:
+	while True:
+            executeAll('rsyncWithServer()','installAPKs()', 'pushData()')
+	    waitForNewDevice()
+    # Skip sending huge data again, only installs the apks    
+    elif '-a' in args:
+        executeAll('pushData()')
+    # Quick help on commands    
+    elif '-h' in args:
+        help()
+    # Show detailed offline help in web browser
+    elif '-hb' in args:
+        if int(len(args)) is 3:
+            os.system("%s /usr/share/aakash/help.html" %(args[args.index('-hb') + 1]))
+        else:
+            print "Missing browser name. Example:"
+            print "   $ aakash -hb firefox"
+    elif '-add' in args:
+        if int(len(args)) is 3 and args[-1].startswith('ftp://') and args[-1].endswith('/aakash'):
+            ftp_url = open(os.getenv('HOME') + '/.aakash','w')
+            ftp_url.write(args[-1])
+            ftp_url.close()
+            print "\nURL updated successfully.\nIf you want to modify URL again, repeat the previous procedure."
+            print "\nNow run 'aakash' again." 
+        else:
+            print "\n\nNot a valid url, please check again."
+            sys.exit(0)
+    # The main execute loop, default application starts here        
+    elif int(len(args)) is 1:
+        while True:
+            executeAll()
+            waitForNewDevice()
+    else:
+        print "Wrong option. Please see help(-h)"
+        print "   $ aakash -h"
+
+
+
+if __name__=="__main__":
+    processArgs()   
