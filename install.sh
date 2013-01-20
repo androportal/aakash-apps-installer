86c193e8714a53e64e8acb77fb13749cce983897
Srikant <usp71084@gmail.com>
2013-01-20 23:44:19 +0530

    removed curlftps echo statements and modified installation text

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Copying files to system locations"
+echo "Installing adb and aakash installer"
 sudo cp aakash-installer.py aakash
 sudo chmod a+x adb aakash
 sudo cp adb /usr/bin
@@ -9,8 +9,9 @@ sudo mkdir -p /usr/share/aakash
 sudo cp help.txt /usr/share/aakash/
 sudo cp help.html /usr/share/aakash/
 
-echo "Done copying. Now installing curlftpfs and rsync from repositories."
-echo "Check your internet connection if this step fails."
+echo "Installation completed successfully !"
+#echo "Done copying. Now installing curlftpfs and rsync from repositories."
+#echo "Check your internet connection if this step fails."
 
 #sudo apt-get install curlftpfs rsync
 
